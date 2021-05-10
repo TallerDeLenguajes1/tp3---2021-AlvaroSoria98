4902fee89659b03439301a3b46cbaf013b1ebf47
AlvaroSoria <alvaro98.cjs@gmail.com>
2021-05-10 17:39:53 -0300

    desarrollo del tp3, no muestra datos

--- tp3_1_1.c
+++ tp3_1_1.c
@@ -19,7 +19,42 @@ struct cliente_t{
     product *producto;//tamaño de este arreglo depende de la variable cantProducApedir
 };
 typedef struct cliente_t client;
+/*Declaracion de Funciones*/
+client *asigMD(client *datos, int *n);
+void CargarCliente(client *datos, int *n);
 
 int main(){
+    srand(time(NULL));
+    int cantClient;
+        printf("Ingrese la cantidad de clientes: ");
+        scanf("%d", &cantClient);
+    client *p_client=asigMD(p_client,&cantClient);
+    CargarCliente(p_client,&cantClient);
+    puts(p_client->producto->tipoProducto[0]);
+    free(p_client);
     return 0;
+}
+
+//Funciones
+client *asigMD(client *memoria, int *n){
+    client *clienteN=(client*)malloc(*n*sizeof(client));
+    return memoria;
+}
+
+void CargarCliente(client *datos, int *n){
+    for(int i=0; i < *n; i++){
+        datos->cliendteID= i+1;
+        datos->nombreCliente=malloc(20*sizeof(char));
+        printf("Ingrese el nombre del cliente: ");
+        gets(datos->nombreCliente);
+        datos->cantProducApedir=rand()%(5-1+1)+1;
+        for (int i = 0; i < datos->cantProducApedir; i++)
+        {
+            datos->producto=(product*)malloc(i*sizeof(product));
+            datos->producto->productoID=i+1;
+            datos->producto->cantidad=rand()%(10-1+1)+1;
+            strcpy(datos->producto->tipoProducto[i],*t_productos[i]);
+        }
+        datos++;
+    }
 }
